99c593013ca156a9b29c80a8e8c80efdff9b9da6
kowsarahammd80 <kowsarahammed80@gmail.com>
2024-04-03 01:44:49 +0600

    importSet

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,10 @@
 import { Roboto_Serif } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/Components/SheardComponents/Navbar/Navbar";
 import Script from "next/script";
+import Navbar from "@/components/SheardComponents/Navbar/Navbar";
 import Footer from "@/components/SheardComponents/Footer/Footer";
+
+
 
 
 const inter = Roboto_Serif({ subsets: ["latin"], weight: ["100","300", "400", "500", "700"] });
